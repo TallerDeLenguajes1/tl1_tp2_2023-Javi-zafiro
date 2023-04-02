55bae1cb5d43a977fcc41638a6f21b55193ffc12
Javi-zafiro <javialexiscortez2@gmail.com>
2023-04-02 20:45:40 -0300

    se agregaron las funciones a tp2_4, punto 4 terminado

--- tp2_4.c
+++ tp2_4.c
@@ -12,6 +12,10 @@ struct compu
     char *tipo_cpu;
 };
 
+void mostrar (struct compu *p);
+void masVieja(struct compu *p);
+void masVeloz(struct compu *p);
+
 int main()
 {
     int j;
@@ -27,6 +31,66 @@ int main()
         j=rand()%5;
         PC[i].tipo_cpu=&tipos[j][10];
     }
-    
+    mostrar(PC);
+    masVieja(PC);
+    masVeloz(PC);
     return 0;
+}
+
+void mostrar (struct compu *p){
+    struct compu *aux;
+    for (int i = 0; i < N; i++)
+    {
+        aux=p;
+        p=p+i;
+        printf("----PC %d----\n",i+1);
+        printf("la velocidad es: %d\n", (*p).velocidad);
+        printf("el año es: %d\n", (*p).anio);
+        printf("la cantidad de nucleos es: %d\n", (*p).cantidad);
+        printf("la marca del cpu es: ");
+        puts((*p).tipo_cpu);
+        printf("\n");
+        p=aux;
+    }
+    
+}
+
+void masVieja(struct compu *p){
+    struct compu *aux;
+    int anio=9999, pc;
+    for (int i = 0; i < N; i++)
+    {
+        aux=p;
+        p=p+i;
+        if ((*p).anio<anio)
+        {
+            pc=i+1;
+            anio=(*p).anio;
+        }
+        p=aux;
+    }
+    printf("la pc mas vieja es la numero %d\n",pc);
+    printf("el año de fabricacion es: %d\n", anio);
+    printf("\n");
+    
+}
+
+void masVeloz(struct compu *p){
+    struct compu *aux;
+    int velMax=0, pc;
+    for (int i = 0; i < N; i++)
+    {
+        aux=p;
+        p=p+i;
+        if ((*p).velocidad>velMax)
+        {
+            pc=i+1;
+            velMax=(*p).velocidad;
+        }
+        p=aux;
+    }
+    printf("la pc mas veloz es la numero %d\n",pc);
+    printf("su velocidad es: %d\n", velMax);
+    printf("\n");
+    
 }
